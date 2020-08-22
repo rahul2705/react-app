e760b28ab11f95b0042cafe6a1dc42e8025ef142
RahulAgrawal <rahul.agrawal@target.com>
2020-08-22 13:15:31 +0530

    Created basic project setup

--- src/App.js
+++ src/App.js
@@ -5,10 +5,15 @@ import './App.css';
 class App extends Component {
   render() {
     return (
+      <div className="App">
         <div className="App-header">
-          <h2>Student Management System</h2>
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
         </div>
-
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
     );
   }
 }
